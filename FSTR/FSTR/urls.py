e1a987bfc3f186c21006dd885619e21c0e56476d
lizard-b <lizardb82@gmail.com>
2024-09-12 06:58:09 +0300

    urls added

--- FSTR/FSTR/urls.py
+++ FSTR/FSTR/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from perevalDB import viewsets
+from . import settings
+
+router = routers.DefaultRouter()
+router.register(r'pereval', viewsets.PerevalViewset)
+router.register(r'user', viewsets.TouristViewset)
+router.register(r'coords', viewsets.CoordinatesViewset)
+router.register(r'level', viewsets.LevelViewset)
+router.register(r'image', viewsets.PerevalImageViewset)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), # ссылка для скачивания файла документации FSTR API.yaml
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
